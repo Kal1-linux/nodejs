b292d5bec2bed6821a77c25b8c7252322c48251a
pratham <jhahiren36@gmail.com>
2023-05-24 23:24:32 +0530

    done

---
+++ express.js
@@ -0,0 +1,17 @@
+var x = require("express");
+var path = require('path');
+var app = x();
+app.use(x.static(path.join(__dirname,"pro")));
+app.get('/',function(req,res){
+	
+    
+    res.sendFile(path.join(__dirname, "pro","index.html"));
+})
+app.get('/spa',function(req,res){
+	
+    
+    res.sendFile(path.join(__dirname, "pro",req.url));
+})
+app.listen(8080,function(){
+})
+
